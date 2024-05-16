a233da8b6279cde1b26f1273f9a073e869973df3
Ludwig-Wredendal <ludwig@wredendal.se>
2024-05-16 15:02:40 +0200

    Testing

--- my-api-project/src/App.js
+++ my-api-project/src/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Willkommen</h1>
+        <h1>Välkommen till Hata Kul</h1>
 
         <WeatherDisplay />
         <DataDisplay />
