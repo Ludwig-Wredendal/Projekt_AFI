3d90e370a4411ecbcf2edb7929905f9f269a8c75
Ludwig-Wredendal <ludwig@wredendal.se>
2024-05-21 13:02:04 +0200

    test

--- my-api-project/src/components/WeatherAndActivity.js
+++ my-api-project/src/components/WeatherAndActivity.js
@@ -4,25 +4,33 @@ import DataDisplay from './DataDisplay';
 
 const WeatherAndActivity = () => {
   const [temperature, setTemperature] = useState(null);
-  const [activityType, setActivityType] = useState('recreational');
+  const [activityType, setActivityType] = useState('');
   const [fetchActivityTrigger, setFetchActivityTrigger] = useState(false);
+  const [participants, setParticipants] = useState('');
+
 
   const determineActivityType = (temp) => {
     let activities;
 
     if (temp < 15) {
-        activities = ['busywork', 'education', 'charity'];
+        activities = ['busywork', 'education', 'relaxation'];
     } else if (temp < 20) {
-        activities = ['cooking', 'diy'];
+        activities = ['cooking', 'diy', 'music'];
     } else if (temp < 25) {
         activities = ['recreational', 'social'];
     } else {
-        activities = ['relaxation', 'music'];
+        activities = ['charity'];
     }
 
     const activityType = activities[Math.floor(Math.random() * activities.length)];
     return activityType;
   };
+
+  const determineNrOfParticipants = (event) => {
+    const value = event.target.value;
+    setParticipants(value);
+  };
+  
 
   useEffect(() => {
     if (temperature !== null) {
@@ -44,6 +52,16 @@ const WeatherAndActivity = () => {
           <button onClick={fetchNewActivity}>New activity</button> {/*Hämtar ny aktivitet från redan bestämt typ*/}
         </div>
       )}
+      <div>
+        <label htmlFor="participants">Number of Participants:</label>
+        <input
+          type="number"
+          id="participants"
+          value={participants}
+          onChange={determineNrOfParticipants}
+          min="1"
+        />
+      </div>
     </div>
   );
 };
